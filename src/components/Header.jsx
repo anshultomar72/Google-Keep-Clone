cb1b53776453ee7c5c4ca7bf1a56fe3d7512b498
anshultomar72 <95484461+anshultomar72@users.noreply.github.com>
2023-10-02 19:41:38 +0530

    Created Simple Todo Website

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Highlight from '@mui/icons-material/Highlight';
+
+function Header(){
+    return (
+        <header>
+      <h1>
+        <Highlight />
+        Keeper
+      </h1>
+    </header>
+    )
+}
+
+export default Header;
